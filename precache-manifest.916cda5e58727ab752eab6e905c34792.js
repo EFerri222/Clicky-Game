ab780b8293e569cccdeab145e2f9cc6e85ee8963
EFerri222 <eferri222@gmail.com>
2019-04-06 20:46:32 -0400

    Updates

---
+++ precache-manifest.916cda5e58727ab752eab6e905c34792.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Clicky-Game/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  },
+  {
+    "revision": "f67a4ac43bdcc79b5136",
+    "url": "/Clicky-Game/static/js/main.cd28adcc.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/Clicky-Game/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "f67a4ac43bdcc79b5136",
+    "url": "/Clicky-Game/static/css/main.92d83d54.chunk.css"
+  },
+  {
+    "revision": "a51a4fd11b4bdba8b7ffd6edef1fe989",
+    "url": "/Clicky-Game/index.html"
+  }
+];
